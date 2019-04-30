3acd1a234098c8016ba90cdd46136f8fe0e3a325
Richard Ma <rich.maaa@gmail.com>
2019-04-30 01:27:00 -0700

    Successfully connect MongoDB

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-
 //routes
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
-
 
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log("Connected to MongoDB successfully"))
 	.catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send("hello world"));
+app.get('/', (req, res) => {
+	res.send("hello world");
+});
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -25,8 +25,9 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 
-
+// production process.env.PORT provided by heroku
 const port = process.env.PORT || 5000;
+
 //starting a socket and lstening for connections
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
