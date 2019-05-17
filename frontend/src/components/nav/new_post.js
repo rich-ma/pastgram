cef29c8913eb857c349b1d167a9b0ce4b284f068
Richard Ma <rich.maaa@gmail.com>
2019-05-16 23:42:21 -0700

    Add esc on new post

--- frontend/src/components/nav/new_post.js
+++ frontend/src/components/nav/new_post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/new_post.css';
 
 class NewPost extends React.Component{
 
@@ -7,12 +8,28 @@ class NewPost extends React.Component{
 		this.state = {
 			currentUser: this.props.currentUser,
 			errors: this.props.errors,
-			text: null,
+			text: '',
 			photoUrl: null
 		}
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleFile = this.handleFile.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 	}
+
+	handleKeyPress(e){
+		if (e.keyCode === 27) {
+			this.props.closeModal();
+		}
+	}
+
+	componentDidMount(){
+		document.addEventListener('keydown', this.handleKeyPress, false);
+	}
+
+	componentWillUnmount(){
+		document.removeEventListener('keydown', this.handleKeyPress, false);
+	}
+
 
 	handleSubmit(e){
 		e.preventDefault();
@@ -26,14 +43,17 @@ class NewPost extends React.Component{
 
 	updateText(e){
 		e.preventDefault();
-		this.setState({text: e.currentUser.value});
+		this.setState({text: e.currentTarget.value});
 	}
 
 	render(){
 		return(
 			<div className='new-post-container' >
+				
 				<div className='new-post-upper'>
-					<img src={this.state.currentUser.avatarUrl} className='new-post-profile' />
+					<div className='new-post-avatar-box'>
+						<img src={this.state.currentUser.avatarUrl} className='new-post-profile' />
+					</div>
 					<input type='text' placeholder='Write a caption...' onChange={e => this.updateText(e)} value={this.state.text} />
 				</div>
 				<div className='new-post-lower'>
