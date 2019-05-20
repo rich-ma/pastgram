bc228406340c6298a30fca88547757b25db82043
Richard Ma <rich.maaa@gmail.com>
2019-05-19 22:51:56 -0700

    Add file_upload

---
+++ services/file_upload.js
@@ -0,0 +1,28 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const keys = require('../config/keys');
+
+aws.config.update({
+	secretAccessKey: keys.secretAccessKey,
+	accessKeyId: keys.accessKeyId,
+	region: 'us-west-1'
+})
+
+const s3 = new aws.S3();
+
+const upload = multer({
+	storage: multerS3({
+		s3: s3,
+		bucket: 'postgram-dev',
+		acl: 'public-read',
+		metadata: function (req, file, cb) {
+			cb(null, {
+				fieldName: file.fieldname
+			});
+		},
+		key: function (req, file, cb) {
+			cb(null, Date.now().toString())
+		}
+	})
+});
